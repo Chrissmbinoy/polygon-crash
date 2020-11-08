02ecaf337793d977738341e26192dbed36d8bae1
Chrissmbinoy <71966593+Chrissmbinoy@users.noreply.github.com>
2020-11-08 19:57:43 +0530

    Add files via upload

---
+++ Box2.js
@@ -0,0 +1,23 @@
+class Box2{
+    constructor(x, y, width, height) {
+        var options = {
+           isStatic:false,
+           'restitution':0.03,
+            'friction':0.3,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y,40,50, options);
+        this.width = 30;
+        this.height = 40;
+     
+        World.add(world, this.body);
+      }
+      display(){
+      
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        fill("pink")
+        rect( 0, 0,40,50);
+        pop();
+      }
+}
